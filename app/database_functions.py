ae43f1029c706edd952043f263adb830ae1f6af1
deycalme6k <allen.nii@icloud.com>
2021-09-10 16:16:35 +0000

    syntax bugs fixed

--- app/database_functions.py
+++ app/database_functions.py
@@ -2,8 +2,8 @@ from app.extensions import db
 from app.models import Voter
 
 
-def mass_import(voter):
-    voter = Voter(name=row[1], email=row[2], index_number=row[3], campus=row[4], level=row[5], election_id=election.id)
+def mass_import(row):
+    voter = Voter(name=row[1], email=row[2], index_number=row[3], campus=row[4], level=row[5], election_id=row[6])
     db.session.add(voter)
     db.session.commit()
 
--- app/functions.py
+++ app/functions.py
@@ -28,18 +28,20 @@ def start_end_election():
     
 
 def save_picture(candidate_name,form_picture):
-    random_hex = secrets.token_hex(3)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = candidate_name + random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/candidate_pictures', picture_fn)
-    form_picture.save(picture_path)
-    
-    output_size = (500, 500) #minimizing the size of the image so it isn't saved so large in the database
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-    
-    return picture_fn
+    app = current_app._get_current_object()
+    with app.app_context():
+        random_hex = secrets.token_hex(3)
+        _, f_ext = os.path.splitext(form_picture.filename)
+        picture_fn = candidate_name + random_hex + f_ext
+        picture_path = os.path.join(app.root_path, 'static/candidate_pictures', picture_fn)
+        form_picture.save(picture_path)
+        
+        output_size = (500, 500) #minimizing the size of the image so it isn't saved so large in the database
+        i = Image.open(form_picture)
+        i.thumbnail(output_size)
+        i.save(picture_path)
+        
+        return picture_fn
 
 
 @async_call
